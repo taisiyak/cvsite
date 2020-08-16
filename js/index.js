8db96b45b8ddb47cdd6c2f5b610a321f6aab35d6
taisiyak <kolentk@gmail.com>
2020-08-16 11:13:06 +0300

    Add responsive to general info

--- js/index.js
+++ js/index.js
@@ -4,31 +4,31 @@ function bodyColor(color) {
 }
 bodyColor("#FFEED8");
 
-const btnCheck = document.querySelector('.btn-check');
-const fieldName = document.querySelector(".wrm-name");
-const fieldEmail = document.querySelector(".wrm-email");
-const fieldSubject = document.querySelector(".wrm-subject");
+// const btnCheck = document.querySelector('.btn-check');
+// const fieldName = document.querySelector(".wrm-name");
+// const fieldEmail = document.querySelector(".wrm-email");
+// const fieldSubject = document.querySelector(".wrm-subject");
 
 
-document.querySelector(".btn-check").addEventListener("click", checkFields);
+// document.querySelector(".btn-check").addEventListener("click", checkFields);
 
-function checkFields() {
+// function checkFields() {
 
-    const fldName = fieldName.value;
-    const fldEmail = fieldEmail.value;
-    const fldSubject = fieldSubject.value;
+//     const fldName = fieldName.value;
+//     const fldEmail = fieldEmail.value;
+//     const fldSubject = fieldSubject.value;
     
-    if (fldName === "" || fldEmail === "" || fldSubject === '') {
+//     if (fldName === "" || fldEmail === "" || fldSubject === '') {
         
-      document.getElementById("text-area").classList.toggle("show");
-    }
-}
+//       document.getElementById("text-area").classList.toggle("show");
+//     }
+// }
 
-const okBtn = document.getElementById('okbtn');
+// const okBtn = document.getElementById('okbtn');
 
-okBtn.addEventListener('click', () =>
-  document.getElementById('text-area').classList.remove('show')
-);
+// okBtn.addEventListener('click', () =>
+//   document.getElementById('text-area').classList.remove('show')
+// );
 
 
 
